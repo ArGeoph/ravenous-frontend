072076c8305c088a7e7868260ec50db2332734e0
Kirill Golubev <arnasay@gmail.com>
2019-01-18 12:30:59 -0700

    Reset.css added to /Public and two React component classes Business and BusinessList and their css files added to /src

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BusinessList.css';
+import { Business } from '../Business/Business';
+
+
+//React component used to nicely render information about restaurants received from YELP API, for now we will use static object business in Business.js
+export class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+            </div>
+        );
+    }
+}
+
+//export { BusinessList };
