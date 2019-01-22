d5a698a1fc8030693500c6ee4e47481d4c3990d0
Kirill Golubev <arnasay@gmail.com>
2019-01-21 22:16:42 -0700

    Basic restaraunts search is implemented using YELP API. App is fully functional now

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './BusinessList.css';
 import { Business } from '../Business/Business';
 
-
 //React component used to nicely render information about restaurants received from YELP API, for now we will use static object business in Business.js
 export class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
                 {this.props.businesses.map(business => {
-                    return <Business business={business} />
+                    return <Business key={business.id} business={business} />
                 })}
             </div>
         );
     }
 }
-
-//export { BusinessList };
-// export default BusinessList;
