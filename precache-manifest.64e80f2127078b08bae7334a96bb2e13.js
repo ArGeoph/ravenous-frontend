c26c55885f053a7256e00e641d2befcfe2a2f7ed
Kirill Golubev <arnasay@gmail.com>
2019-10-18 17:31:09 -0600

    Updates

---
+++ precache-manifest.64e80f2127078b08bae7334a96bb2e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7f51b0d8fcd0457b09a4b618eadfab",
+    "url": "/search-restaurants/index.html"
+  },
+  {
+    "revision": "442d5ef38dd4bf0369a3",
+    "url": "/search-restaurants/static/css/main.190141cf.chunk.css"
+  },
+  {
+    "revision": "18b3f149df75ea73cdae",
+    "url": "/search-restaurants/static/js/2.24ef3863.chunk.js"
+  },
+  {
+    "revision": "442d5ef38dd4bf0369a3",
+    "url": "/search-restaurants/static/js/main.1e1bf0d8.chunk.js"
+  },
+  {
+    "revision": "c57e54c5ca6920fda86b",
+    "url": "/search-restaurants/static/js/runtime-main.7ce1454f.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/search-restaurants/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/search-restaurants/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/search-restaurants/precache-manifest.57cb8fc63a601085a6e90d78503cb532.js"
+  "/search-restaurants/precache-manifest.64e80f2127078b08bae7334a96bb2e13.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/search-restaurants/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/search-restaurants/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
